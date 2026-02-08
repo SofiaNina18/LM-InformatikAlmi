47b1c36d9bfd1dffe4d2ebe49409d7e8f3ee78ed
SofiaNina18 <Snina.work@gmail.com>
2026-02-08 22:59:47 +0100

    cambios en el diseño de menu.css, cambios en crear submenus, y enlazar con index.php

--- menu.php
+++ menu.php
@@ -10,14 +10,26 @@ if (session_status() === PHP_SESSION_NONE) {
     <nav class="contenido">
         <ul class="lista">
             <li><a href="servicios.php">Servicios</a></li>
-            <li><a href="tecnologias.php">Tecnología</a></li>
+            
+            <li class="lista-abajo">
+                <a href="tecnologias.php" class="boton">Tecnología ▾</a>
+                <div class="menu-tecnologia">
+                    <a href="tecnologias.php#portatil">Portátiles</a>
+                    <a href="tecnologias.php#consola">Consolas</a>
+                    <a href="tecnologias.php#grafica">Gráficas</a>
+                    <a href="tecnologias.php#placabase">Placas Base</a>
+                    <a href="tecnologias.php#escritorio">Escritorios</a>
+                    <a href="tecnologias.php#monitor">Monitores</a>
+                </div>
+            </li>
+
             <li><a href="proyectos.php">Proyectos</a></li>
             <li><a href="contacto.php">Contacto</a></li>
             
             <?php if (isset($_SESSION['usuario'])): ?>
-                <li><a href="#" class="iniciar">Cerrar Sesión</a></li>
+                <li><a href="cerrar-sesion.php" class="iniciar cerrar">Cerrar Sesión</a></li>
             <?php else: ?>
-                <li><a href="#" class="iniciar">Iniciar Sesion</a></li>
+                <li><a href="iniciar-usuario.php" class="iniciar">Iniciar Sesion</a></li>
             <?php endif; ?>
         </ul>
     </nav>
