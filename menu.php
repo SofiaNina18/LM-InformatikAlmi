3f13c61b1a7391c9ec7df6a48a79e260f2c69355
SofiaNina18 <Snina.work@gmail.com>
2026-02-09 13:48:50 +0100

    cambios en el menu

--- menu.php
+++ menu.php
@@ -5,6 +5,8 @@ if (session_status() === PHP_SESSION_NONE) {
 
 include_once 'bbdd.php';
 
+$pagina_actual = basename($_SERVER['PHP_SELF']);
+
 $sql_menu = "SELECT * FROM CATEGORIAS ORDER BY ID_CATEGORIA ASC";
 $stmt_menu = oci_parse($conexion, $sql_menu);
 oci_execute($stmt_menu);
@@ -16,10 +18,10 @@ oci_execute($stmt_menu);
     </div>
     <nav class="contenido">
         <ul class="lista">
-            <li><a href="servicios.php">Servicios</a></li>
+            <li><a href="servicios.php" class="<?php echo ($pagina_actual == 'servicios.php') ? 'activar-menu' : ''; ?>">Servicios</a></li>
 
             <li class="lista-abajo">
-                <a href="tecnologias.php" class="boton">Tecnología ▾</a>
+                <a href="tecnologias.php" class="<?php echo ($pagina_actual == 'tecnologias.php') ? 'boton' : ''; ?>">Tecnología ▾</a>
                 <div class="menu-tecnologia">
                     <?php
                     while ($cat = oci_fetch_assoc($stmt_menu)):
@@ -32,16 +34,16 @@ oci_execute($stmt_menu);
                     <?php endwhile; ?>
                 </div>
             </li>
-            <li><a href="proyectos.php">Proyectos</a></li>
-            <li><a href="contacto.php">Contacto</a></li>
+
+            <li><a href="proyectos.php" class="<?php echo ($pagina_actual == 'proyectos.php') ? 'activar-menu' : ''; ?>">Proyectos</a></li>
+            <li><a href="contacto.php" class="<?php echo ($pagina_actual == 'contacto.php') ? 'activar-menu' : ''; ?>">Contacto</a></li>
 
             <?php if (isset($_SESSION['usuario'])): ?>
                 <li><a href="anadir_producto.php">Añadir Producto</a></li>
                 <li><a href="gestionar_productos.php">Gestionar Productos</a></li>
-
                 <li><a href="cerrar-sesion.php" class="iniciar cerrar">Cerrar Sesión</a></li>
             <?php else: ?>
-                <li><a href="iniciar-usuario.php" class="iniciar">Iniciar Sesion</a></li>
+                <li><a href="iniciar-usuario.php" class="iniciar">Iniciar Sesión</a></li>
             <?php endif; ?>
         </ul>
     </nav>
