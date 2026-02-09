d435e9bea73c736de37287d29cb4abb70e0f28d2
SofiaNina18 <Snina.work@gmail.com>
2026-02-09 13:52:29 +0100

    cambios
    cambios en index

--- index.php
+++ index.php
@@ -23,14 +23,15 @@ include_once 'bbdd.php'
 
         <section class="fondo">
             <video autoplay muted loop class="fondo-video">
-                <source src="videos/fondo.mp4" type="video/mp4">
+                <source src="videos/Fondo.mp4" type="video/mp4">
                 Tu navegador no soporta el video.
             </video>
             <div class="overlay"></div>
 
             <div class="titulo">
                 <h1 class="bienvenido">Bienvenido a SAYO</h1>
-                <p class="texto-animado">Desarrollamos software y aplicaciones a medida para empresas y particulares.</p>
+                <p class="texto-animado">Soluciones a medida en hardware y componentes informáticos <br>
+                    certificados para potenciar tu productividad y eficiencia.</p>
             </div>
         </section>
 
