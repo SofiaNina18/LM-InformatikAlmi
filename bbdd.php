93444638c1c151f145df042ac3b4959b237a6b39
SofiaNina18 <Snina.work@gmail.com>
2026-02-09 11:32:55 +0100

    cambio en bbdd

--- bbdd.php
+++ bbdd.php
@@ -2,18 +2,16 @@
 $user = "InformatikAlmi";
 $password = "Almi12345";
 $bbdd = "192.168.0.152/ORCLCDB"; 
-// $bbdd = "192.168.0.136/ORCLCDB"; 
+//$bbdd = "192.168.0.136/ORCLCDB"; 
 //$bbdd = "192.168.1.131/ORCLCDB";
 
 $conexion = oci_connect($user, $password, $bbdd, 'AL32UTF8');
 
 if (!$conexion) {
     $e = oci_error();
-    // En producción no deberíamos mostrar el error real al usuario, pero para desarrollo está bien
     die("Error de conexión a Oracle: " . $e['message']);
 }
 
-// Opcional: Función helper para cerrar conexión
 function cerrarConexion($conn) {
     oci_close($conn);
 }
