2f1ac21b108cf719749c2cbceb7afe0635beef8c
SofiaNina18 <Snina.work@gmail.com>
2026-02-09 11:43:02 +0100

    tecnologia css y php terminados

--- tecnologias.php
+++ tecnologias.php
@@ -1,6 +1,8 @@
 <?php 
 include_once 'bbdd.php'; 
 if (session_status() === PHP_SESSION_NONE) session_start();
+
+$filtro_categoria = isset($_GET['categoria']) ? $_GET['categoria'] : null;
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -13,65 +15,60 @@ if (session_status() === PHP_SESSION_NONE) session_start();
     <link rel="stylesheet" href="css/tecnologias.css">
 </head>
 
-<body class="pag-tecnologia">
+<body class="tecnologia">
 
     <?php include 'menu.php'; ?>
 
-    <header class="cabecera-tech">
+    <header class="titulo-tecnologia">
         <h1>CATÁLOGO SAYO</h1>
-        <p class="subtitulo-tech">Hardware de alto rendimiento</p>
+        <p class="texto-abajo">Hardware de alto rendimiento</p>
     </header>
 
-    <main class="contenedor-principal">
+    <main class="categorias-productos">
         <?php
-       
-        $sql_cat = "SELECT * FROM CATEGORIAS ORDER BY id_categoria ASC";
-        $stmt_cat = oci_parse($conexion, $sql_cat);
+
+        if ($filtro_categoria) {
+            $sql_cat = "SELECT * FROM CATEGORIAS WHERE ID_CATEGORIA = :id";
+            $stmt_cat = oci_parse($conexion, $sql_cat);
+            oci_bind_by_name($stmt_cat, ":id", $filtro_categoria);
+        } else {
+            $sql_cat = "SELECT * FROM CATEGORIAS ORDER BY ID_CATEGORIA ASC";
+            $stmt_cat = oci_parse($conexion, $sql_cat);
+        }
+        
         oci_execute($stmt_cat);
 
         while ($cat = oci_fetch_assoc($stmt_cat)) {
             $id_cat = $cat['ID_CATEGORIA'];
             $nombre_cat = $cat['NOMBRE_CATEGORIA'];
 
-           
             $ancla = strtolower(str_replace(['á','é','í','ó','ú',' '], ['a','e','i','o','u',''], $nombre_cat));
-            if(strpos($ancla, 'grafica') !== false) $ancla = 'grafica';
 
             echo "<section id='$ancla'>"; 
 
-            
-            $titulo_bonito = $nombre_cat;
-            if ($nombre_cat == 'Portátil') $titulo_bonito = "Nuestros Portátiles";
-            elseif ($nombre_cat == 'Consola') $titulo_bonito = "Nuestras Consolas";
-            elseif ($nombre_cat == 'Tarjeta Gráfica') $titulo_bonito = "Tarjetas Gráficas";
-            elseif ($nombre_cat == 'Placa Base') $titulo_bonito = "Placas Base";
-            elseif ($nombre_cat == 'Escritorio') $titulo_bonito = "Escritorios Gaming";
-            elseif ($nombre_cat == 'Monitor') $titulo_bonito = "Monitores";
+            echo "<h2 class='titulo-seccion'>" . strtoupper($nombre_cat) . "</h2>";
 
-            echo "<h2 class='titulo-seccion'>$titulo_bonito</h2>";
-
-            
             $sql_prod = "SELECT * FROM PRODUCTOS WHERE id_categoria = :id";
             $stmt_prod = oci_parse($conexion, $sql_prod);
             oci_bind_by_name($stmt_prod, ":id", $id_cat);
             oci_execute($stmt_prod);
 
-            echo "<div class='grid-3-columnas'>";
+            echo "<div class='3-columnas'>";
 
             $hay_productos = false;
             while ($prod = oci_fetch_assoc($stmt_prod)) {
                 $hay_productos = true;
-                $img = $prod['IMAGEN'] ? $prod['IMAGEN'] : 'images/sin_imagen.png';
+                $img = !empty($prod['IMAGEN']) ? $prod['IMAGEN'] : 'images/sin_imagen.png';
                 
                 echo "
                 <article class='tarjeta-producto'>
-                    <div class='img-wrap'>
+                    <div class='img-contenido'>
                         <img src='$img' alt='Producto'>
                     </div>
-                    <div class='contenido-tarjeta'>
+                    <div class='contenido-tecnologia'>
                         <h3>{$prod['TITULO']}</h3>
                         <p class='resumen'>{$prod['RESUMEN']}</p>
-                        <a href='ver_producto.php?id={$prod['ID_PRODUCTO']}' class='btn-ver-mas'>Ver más ></a>
+                        <a href='ver_producto.php?id={$prod['ID_PRODUCTO']}' class='ver-mas'>Ver más ></a>
                     </div>
                 </article>
                 ";
